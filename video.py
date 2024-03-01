327ad10616a09fdef01eea7295d1a28390cf820a
DHG-Andy <151802590+DHG-Andy@users.noreply.github.com>
2024-03-01 18:14:06 +0800

    add video search and insert video embedding

--- video.py
+++ video.py
@@ -18,10 +18,9 @@ query_pipe = (
     .map('frames', ('labels', 'scores', 'features'),
          ops.action_classification.pytorchvideo(model_name=MODEL, skip_preprocess=True))
     .map('features', 'result',
-         ops.ann_search.milvus_client(uri='https://in01-a0666c0cb6b20dc.aws-us-west-2.vectordb.zillizcloud.com:19535',
-                                      token='60f73dd2e01fd8335eab3605911d381f5a9b872be8fe8a170e79b21a435f1b39f05f44fd1e683f85b1393040a988bee5e66f200e',
+         ops.ann_search.milvus_client(uri=MILVUS_URI,
+                                      token=MILVUS_TOKEN,
                                       collection_name=DEFAULT_TABLE, limit=10))
-    .map('result', 'candidates', lambda x: [id_video[i[0]] for i in x])
     .output('path', 'candidates')
 )
 
